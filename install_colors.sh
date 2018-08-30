5aaf954dbdfc402e7184c0a0f2f8439be61277f7
NaabZer <anton.gefvert@gmail.com>
2018-08-30 13:50:03 +0200

    Add rofi, update polybar with network selection (using rofi)

--- install_colors.sh
+++ install_colors.sh
@@ -3,9 +3,18 @@
 . base16colorscheme.conf
 
 # Download termite colors
-colorurl="https://raw.githubusercontent.com/khamer/base16-termite/master/themes/base16-${colorscheme}.config"
+termiteurl="https://raw.githubusercontent.com/khamer/base16-termite/master/themes/base16-${colorscheme}.config"
 echo "downloading colorscheme for termite"
-$(curl $colorurl > terminal/termite/colors)
+$(curl $termiteurl > terminal/termite/colors)
 $(cat terminal/termite/base terminal/termite/colors > terminal/termite/config)
+
+
+# Download rofi colors
+rofiurl="https://raw.githubusercontent.com/0xdec/base16-rofi/master/themes/base16-${colorscheme}.config"
+echo "downloading colorscheme for rofi"
+$(curl $rofiurl > gui/rofi/colors)
+$(cat gui/rofi/base gui/rofi/colors > gui/rofi/config)
+
+    
 
 
