e010560ba70be1aab4e3d3d85438a405a8ee1f69
NaabZer <anton.gefvert@gmail.com>
2018-10-16 16:45:32 +0200

    Change colorscheme to base16-apathy

--- gui/polybar/spotify.sh
+++ gui/polybar/spotify.sh
@@ -2,6 +2,6 @@
 scriptpath=$(dirname $0)
 scriptpath+="/get_song.sh"
 
-(zscroll -u t -n t -d 0.2 -l 20 -M "playerctl status 2> /dev/null" -m "Paused" "-s f -b ' '"\
+(zscroll -u t -n t -d 0.2 -l 30 -M "playerctl status 2> /dev/null" -m "Paused" "-s f -b ' '"\
     -m "Playing" "-s t -b ' '" "bash  $scriptpath") &
 wait
